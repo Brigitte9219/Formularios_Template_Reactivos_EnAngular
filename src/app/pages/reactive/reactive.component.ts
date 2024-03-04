dadacc30945ab52f939ae3bc1527b9178d826e60
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-03-04 14:19:08 -0500

    Creación del formulario FormGroup

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent {
+
+  forma!: FormGroup;
+
+  constructor(private fb:FormBuilder){
+  this.crearFormulario();
+  }
+
+  crearFormulario(){
+    this.forma = this.fb.group({
+    nombre:['brigitte'],
+    apellido:['padilla'],
+    correo:['brillo@gmail.com']
+  });
+  }
+
+  guardar(){
+    console.log(this.forma);
+  }
+
+
 
 }
